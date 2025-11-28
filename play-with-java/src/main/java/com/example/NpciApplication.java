93fc144843f38e8f547147c421e0defb00b4051e
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-11-28 12:26:02 +0530

    fix

--- play-with-java/src/main/java/com/example/NpciApplication.java
+++ play-with-java/src/main/java/com/example/NpciApplication.java
@@ -4,9 +4,9 @@ public class NpciApplication {
 
     public static void main(String[] args) {
 
-        PaymentService upiPaymentService = new UPIPayment("s1", "r1", 100.00, "1234");
+        PaymentService upiPaymentService = new UPIPayment("s1@oksbi", "r1@okicic", 100.00, "1234");
         upiPaymentService.executePayment();
-        PaymentService impsPaymentService = new IMPSPayment("9876543210", "0123456789", "CAN00163", 100.00);
+        PaymentService impsPaymentService = new IMPSPayment("9876543210", "0123456789", "CAN00000163", 100.00);
         impsPaymentService.executePayment();
 
     }
