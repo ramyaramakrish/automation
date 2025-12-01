b118b75259ba13524a15adecceb12c0113a336ed
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-12-01 11:23:28 +0530

    static members

---
+++ play-with-java/src/main/java/com/example/mini_example/Quiz.java
@@ -0,0 +1,10 @@
+package com.example.mini_example;
+
+public class Quiz {
+    int i = 100;
+
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+        System.out.println(quiz.i);
+    }
+}
---
+++ play-with-java/src/main/java/com/example/mini_example/Variable_Use_In_Method.java
@@ -0,0 +1,28 @@
+package com.example.mini_example;
+
+
+class SomeClass {
+    static int staticVar = 1;
+    int instanceVar = 2;
+
+    static void staMethod() {
+        System.out.println(staticVar);
+//        System.out.println(instanceVar);//---> Error
+    }
+
+    void insMethod() {
+        System.out.println(staticVar);
+        System.out.println(instanceVar);
+    }
+}
+
+public class Variable_Use_In_Method {
+    public static void main(String[] args) {
+
+        SomeClass.staMethod();
+
+        SomeClass obj = new SomeClass();
+        obj.insMethod();
+
+    }
+}
